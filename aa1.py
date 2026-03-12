9e886ac18efc1a6328fd7cf21408e5bf7c329e82
hadi-20066 <hjunkz123@gmail.com>
2026-03-12 09:47:06 +0530

    Add files via upload

---
+++ aa1.py
@@ -0,0 +1,13 @@
+def reflex_vacuum_agent(location, status):
+    if status == "Dirty":
+        return "Suck"
+    elif location == "A":
+        return "Right"
+    elif location == "B":
+        return "Left"
+
+location = "A"
+status = "Dirty"
+
+action = reflex_vacuum_agent(location, status)
+print(f"Location: {location}, Status: {status}, Action: {action}")
---
+++ aa3.py
@@ -0,0 +1,17 @@
+class RationalAgent:
+    def _init_(self, goal):
+        self.position = 0
+        self.goal = goal
+    
+    def move_towards_goal(self):
+        while self.position < self.goal:
+            self.position += 1
+            print(f"Current Position: {self.position}")
+        print("Reached the goal!")
+
+goal_position = 5
+
+agent = RationalAgent(goal_position)
+
+agent.move_towards_goal()
+
